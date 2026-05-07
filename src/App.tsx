f068f95ed4928343b1adb84e58a874e83fd2a631
pjsk02 <pulipati.j@northeastern.edu>
2026-05-07 12:51:37 -0400

    feat(app): wire Background and ApiKeyGate into root App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import './index.css'
+import Background from './components/Background'
+import ApiKeyGate from './components/ApiKeyGate'
 import IndexPage from './pages/index'
 
 export default function App() {
-  return <IndexPage />
+  return (
+    <ApiKeyGate>
+      <div className="relative min-h-screen bg-gray-50 dark:bg-gray-900">
+        <Background />
+        <div className="relative z-10">
+          <IndexPage />
+        </div>
+      </div>
+    </ApiKeyGate>
+  );
 }
